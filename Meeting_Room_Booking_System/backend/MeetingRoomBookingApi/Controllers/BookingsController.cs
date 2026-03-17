dcdc2eb6f954df71aec11bd4e85c42f48b0eba10
Patel Smit Vishalkumar <23002170110133@mail.ljku.edu.in>
2026-03-17 10:28:14 +0530

    Update BookingsController.cs

--- Meeting_Room_Booking_System/backend/MeetingRoomBookingApi/Controllers/BookingsController.cs
+++ Meeting_Room_Booking_System/backend/MeetingRoomBookingApi/Controllers/BookingsController.cs
@@ -79,8 +79,10 @@ namespace MeetingRoomBookingApi.Controllers
 //
             // booking.StartTime = booking.StartTime.ToUniversalTime();
             // booking.EndTime = booking.EndTime.ToUniversalTime();
-            booking.StartTime = booking.StartTime.ToUniversalTime();
-            booking.EndTime = booking.EndTime.ToUniversalTime();
+            // booking.StartTime = booking.StartTime.ToUniversalTime();
+            // booking.EndTime = booking.EndTime.ToUniversalTime();
+            booking.StartTime = DateTime.SpecifyKind(booking.StartTime.ToUniversalTime(), DateTimeKind.Utc);
+            booking.EndTime = DateTime.SpecifyKind(booking.EndTime.ToUniversalTime(), DateTimeKind.Utc);
             // booking.StartTime = DateTime.SpecifyKind(booking.StartTime, DateTimeKind.Utc);
             // booking.EndTime = DateTime.SpecifyKind(booking.EndTime, DateTimeKind.Utc)
             // booking.StartTime = DateTime.SpecifyKind(booking.StartTime, DateTimeKind.Utc);
